830910cfe78a04879d2834da2c79e1820a80a1b1
kyliepost <kylie.postlethwaite00@gmail.com>
2021-11-01 13:46:28 -0500

    fixed my models location

--- levelup/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .gamer import Gamer
-from .gameType import GameType
-from .game import Game
-from .event import Event
-from .eventGamer import EventGamer
---
+++ levelupapi/models/__init__.py
@@ -0,0 +1,5 @@
+from .gamer import Gamer
+from .gameType import GameType
+from .game import Game
+from .event import Event
+from .eventGamer import EventGamer
