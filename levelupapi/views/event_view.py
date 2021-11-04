562731046bb6f7577abb90a62dda87194e116d29
kyliepost <kylie.postlethwaite00@gmail.com>
2021-11-04 09:48:51 -0500

    event serializer

---
+++ levelupapi/views/event_view.py
@@ -0,0 +1,155 @@
+"""View module for handling requests about games"""
+from django.core.exceptions import ValidationError
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from levelupapi.models import Game, Event, Gamer
+
+
+class EventView(ViewSet):
+    """Level up games"""
+
+    def create(self, request):
+        """Handle POST operations
+        Returns:
+            Response -- JSON serialized game instance
+        """
+
+        # Uses the token passed in the `Authorization` header
+        gamer = Gamer.objects.get(user=request.auth.user)
+
+        # Use the Django ORM to get the record from the database
+        # whose `id` is what the client passed as the
+        # `gameTypeId` in the body of the request.
+        game = Game.objects.get(pk=request.data["gameId"])
+
+        # Try to save the new game to the database, then
+        # serialize the event instance as JSON, and send the
+        # JSON as a response to the client request
+        try:
+            # Create a new Python instance of the Event class
+            # and set its properties from what was sent in the
+            # body of the request from the client.
+            event = Event.objects.create(
+                description=request.data["description"],
+                date=request.data["date"],
+                time=request.data["time"],
+                organizer=gamer,
+                game = game
+            )
+            serializer = EventSerializer(event, context={'request': request})
+            return Response(serializer.data)
+
+        # If anything went wrong, catch the exception and
+        # send a response with a 400 status code to tell the
+        # client that something was wrong with its request data
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single event
+        Returns:
+            Response -- JSON serialized event instance
+        """
+        try:
+            # `pk` is a parameter to this function, and
+            # Django parses it from the URL route parameter
+            #   http://localhost:8000/events/2
+            #
+            # The `2` at the end of the route becomes `pk`
+            event = Event.objects.get(pk=pk)
+            serializer = EventSerializer(event, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def update(self, request, pk=None):
+        """Handle PUT requests for a event
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        gamer = Gamer.objects.get(user=request.auth.user)
+        game = Game.objects.get(pk=request.data["gameId"])
+
+        # Do mostly the same thing as POST, but instead of
+        # creating a new instance of Game, get the game record
+        # from the database whose primary key is `pk`
+        event = Event.objects.get(pk=pk)
+        event.description = request.data["description"]
+        event.date = request.data["date"]
+        event.time = request.data["time"]
+        event.game = game
+        event.organizer = gamer
+
+        event.save()
+
+        # 204 status code means everything worked but the
+        # server is not sending back any data in the response
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single game
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            event = Event.objects.get(pk=pk)
+            event.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Event.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def list(self, request):
+        """Handle GET requests to games resource
+        Returns:
+            Response -- JSON serialized list of games
+        """
+        # Get all game records from the database
+        events = Event.objects.all()
+
+        # Support filtering games by type
+        #    http://localhost:8000/games?type=1
+        #
+        # That URL will retrieve all tabletop games
+        # game_type = self.request.query_params.get('type', None)
+        # if game_type is not None:
+        #     games = games.filter(game_type__id=game_type)
+
+        serializer = EventSerializer(
+            events, many=True, context={'request': request})
+        return Response(serializer.data)
+
+class EventUserSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = get_user_model()
+        fields = ['first_name', 'last_name']
+
+class EventGamerSerializer(serializers.ModelSerializer):
+    user = EventUserSerializer(many=False)
+    class Meta:
+        model = Gamer
+        fields = ['user']
+
+class EventSerializer(serializers.ModelSerializer):
+    """JSON serializer for games
+    
+    Arguments:
+        serializer type
+    """
+
+    organizer = EventGamerSerializer(many=False)
+    class Meta:
+        model = Event
+        fields = ('id', 'game', 'description', 'date', 'time', 'organizer')
+        depth = 1
